bdfd8f24b4a405ac31766813b9a0f8d7da897e3a
Lior Sinai <sinailior@gmail.com>
2020-10-22 14:57:26 +0200

    Initial commit of the NonogramSolver

---
+++ Nonogram.cpp
@@ -0,0 +1,182 @@
+/*
+ *  Created on: 22 October 2020
+ *      Author: Lior Sinai
+ * Description: 
+ */
+
+#include "Nonogram.h"
+
+Nonogram::Nonogram(std::vector<std::vector<int>> runs_row_, std::vector<std::vector<int>> runs_col_)
+{   
+    runs_row = runs_row_;
+    runs_col = runs_col_;
+    n_rows = runs_row.size();
+    n_cols = runs_col.size();
+
+    for (int i{0}; i < n_rows; i++){
+        std::vector<int> row;
+        for (int j{0}; j < n_cols; j++){
+            row.push_back(EITHER);
+        }
+        grid.push_back(row);
+    }
+}
+
+std::vector<int> Nonogram::get_column(std::vector<std::vector<int>> grid, int j){
+    std::vector<int> column;
+    for (int i{0}; i < n_rows; i++){
+        column.push_back(grid[i][j]);
+    }
+    return column;
+}
+
+int max_length (std::vector<std::vector<int>> array){
+    int length {0};
+    for (auto vec: array){
+        if (vec.size() > length){
+            length = vec.size();
+        }
+    }
+    return length;    
+}
+
+
+std::vector<std::vector<std::string>> Nonogram::make_box(std::vector<std::vector<int>> grid, std::string symbols)
+{   
+    /* make a box which includes the instructions for the Nonogram */
+    std::vector<std::vector<std::string>> box;
+    int row_max_length = max_length(Nonogram::runs_row);
+    int col_max_length = max_length(Nonogram::runs_col);
+
+    for (int i{0}; i < col_max_length; ++i){
+        std::vector<std::string> row;
+        for (int j{0}; j < row_max_length + n_cols -1; ++j){
+            if (j >= row_max_length && runs_col[j - row_max_length].size() >= col_max_length - i){
+                auto runs = runs_col[j - row_max_length];
+                int r = runs[runs.size() - (col_max_length - i)];
+                row.push_back(std::to_string(r)); 
+            }
+            else{
+                std::string s (1, symbols[DUMMY]);
+                row.push_back(s); // push back a dummy variable
+            }
+        }
+        box.push_back(row);
+    }
+    for (int i{0}; i < n_rows; ++i){
+        std::vector<std::string> row;
+        for (int j{0}; j < row_max_length; ++j){
+            if (runs_row[i].size() >= row_max_length - j){
+                int r = runs_row[i][runs_row[i].size() - (row_max_length - j)];
+                row.push_back(std::to_string(r));
+            } 
+            else{
+                std::string s (1, symbols[DUMMY]);
+                row.push_back(s); // push back a dummy variable
+            }            
+        }
+        for (int j{0}; j < n_cols; ++j){
+            std::string s (1, symbols[grid[i][j]]);
+            row.push_back(s);
+        }
+        box.push_back(row);
+    }
+
+
+    return box;
+}
+
+void Nonogram::show_grid(std::vector<std::vector<int>> &grid, bool show_instructions, bool to_file, std::string symbols)
+{    
+    /* Print nonogram to screen and possible a file as well*/
+    std::ofstream outfile;
+    if (to_file){
+        // construct file name
+        std::string base = "nonogram_";
+        std::string ext = ".txt";
+        // get current and date
+        time_t  now = time(0); // current time
+        struct tm * timeinfo;
+        char buffer [80];
+        time (&now);
+        timeinfo = localtime (&now);
+        strftime (buffer,80,"%Y%m%d_%H%M",timeinfo);
+        // open (and create) the file
+        std::string file_name = base + buffer + ext;
+        outfile.open (file_name);
+    }
+
+    if (show_instructions){
+        std::vector<std::vector<std::string>> box = make_box(grid);
+        for (int i{0}; i< box.size(); i++){
+            for (int j{0}; j < box[0].size(); j++){
+                std::cout << box[i][j] << "  ";
+                if (to_file){
+                    outfile << box[i][j] << " ";
+                }
+            }
+            std::cout << "\n";
+            if (to_file){
+                outfile << "\n";
+            }
+        }
+    }
+    else{ 
+        for (int i{0}; i < n_rows; i++){
+            for (int j{0}; j < n_cols; j++){
+                std::cout << symbols[grid[i][j]] << " ";
+                if (to_file){
+                    outfile << symbols[grid[i][j]] << " ";
+                }
+            }
+            std::cout << "\n";
+            if (to_file){
+                outfile << "\n";
+            }
+        }
+    }
+    std::cout << std::endl;
+    if (to_file){
+        outfile.close();
+    }
+}
+
+
+std::vector<int> Nonogram::get_sequence(std::vector<int> line){
+    std::vector<int> sequence;
+    bool on_blank = true;
+    for (int cell: line){
+        if (cell == cell_types::BOX && on_blank){
+            sequence.push_back(1);
+            on_blank = false;
+        }
+        else if (cell == cell_types::BOX && !on_blank){
+            sequence.back() += 1;
+        }
+        else { //BLANK or EITHER
+            on_blank = true;
+        }
+    } 
+    return sequence;
+}
+
+
+bool Nonogram::is_valid_line(std::vector<int> line, std::vector<int> target_runs){
+    std::vector<int> sequence = get_sequence(line);
+    return sequence == target_runs;
+}
+
+bool Nonogram::is_valid_grid(std::vector<std::vector<int>> &grid)
+{
+    for (int i{0}; i < n_rows; i++){
+        if (!is_valid_line(grid[i], runs_row[i])){
+            return false;
+        }
+    } 
+    for (int j{0}; j < n_cols; j++){
+        if (!this->is_valid_line(get_column(grid, j), this->runs_col[j])){
+            return false;
+        }
+    }
+    return true;
+}
---
+++ Nonogram.h
@@ -0,0 +1,59 @@
+/*
+ *  Created on: 22 October, 2020
+ *      Author: Lior Sinai
+ * Description: A nonogram class
+ */
+
+
+#ifndef NONOGRAM_H
+#define NONOGRAM_H
+
+#include <vector>
+#include <tuple>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <assert.h>
+#include <time.h>
+#include <ctime>
+
+class Nonogram
+{
+private:
+    std::vector<std::vector<int>> runs_row;
+    std::vector<std::vector<int>> runs_col;
+    std::vector<std::vector<int>> grid; 
+
+    int n_rows;
+    int n_cols;
+    int guesses {0};
+
+    std::vector<int> get_column(std::vector<std::vector<int>> grid, int j);
+    std::vector<std::vector<std::string>> make_box(std::vector<std::vector<int>> grid, std::string symbols ="x#.?-");
+public:
+	/// Constructor 
+	Nonogram(std::vector<std::vector<int>> runs_row_, std::vector<std::vector<int>> runs_col_);
+
+
+    // setters and getters
+    int get_n_rows(){ return n_rows;} 
+    int get_n_cols(){ return n_cols;}  
+    int get_guesses(){return guesses;}
+    void set_grid(std::vector<std::vector<int>> &new_grid){grid = new_grid;}
+    std::vector<std::vector<int>> &get_runs_row() {return runs_row;}
+    std::vector<std::vector<int>> &get_runs_col() {return runs_col;}
+    std::vector<std::vector<int>> &get_grid() {return grid;}
+
+    // useful functions
+    void show_grid(std::vector<std::vector<int>> &grid, bool show_instructions=false, bool to_file=false, std::string symbols ="x#.?-");
+    std::vector<int> get_sequence(std::vector<int> line);
+    bool is_valid_line(std::vector<int> line, std::vector<int> target_runs);
+    bool is_valid_grid(std::vector<std::vector<int>> &grid);
+    
+
+
+};
+
+enum cell_types {BOX=1, BLANK=2, EITHER=3, DUMMY=4};
+
+#endif // NONOGRAM_H
